25993b37e19b5bb0ac1d9542e6295beabdfe162a
kidmeier <kid.meier@gmail.com>
2010-07-11 21:45:17 -0400

    + fix to handle C++-style single-line comments in .md5mesh files + some minor parsing fixes

--- src/res.md5.c
+++ src/res.md5.c
@@ -4,20 +4,33 @@
 #include "res.md5.h"
 #include "math.vec.h"
 
+static
+parse_p ff( parse_p P ) {
+
+	P = skipws(P);
+	if( '/' == lookahead(P, 0)
+	    && '/' == lookahead(P, 1) )
+		return skipws(parsync( P, '\n', NULL));
+
+	return P;
+
+}
+
 static 
 md5model_p parse_header( parse_p P ) {
 
-	if( !parsok( match( skipws(P), "MD5Version") ) )
+	if( !parsok( match( ff(P), "MD5Version") ) )
 		return NULL;
 
 	int version; integer( P, &version );
 	if( !parsok(P) || 10 != version ) return NULL;
-	if( !parsok( match( skipws(P), "commandline" ) ) ) return NULL;
-	if( !parsok( qstring( P, NULL, NULL ) ) ) return NULL;
-	if( !parsok( match( skipws(P), "numJoints" ) ) ) return NULL;
-	int n_joints; integer( P, &n_joints );
+	if( !parsok( match( ff(P), "commandline" ) ) ) return NULL;
+	if( !parsok( qstring( ff(P), NULL, NULL ) ) ) return NULL;
+	if( !parsok( match( ff(P), "numJoints" ) ) ) return NULL;
+	int n_joints; integer( ff(P), &n_joints );
 	if( !parsok(P) ) return NULL;
-	int n_meshes; integer( P, &n_meshes );
+	if( !parsok( match( ff(P), "numMeshes" ) ) ) return NULL;
+	int n_meshes; integer( ff(P), &n_meshes );
 	if( !parsok(P) ) return NULL;
 
 	md5model_p mdl = new( NULL, md5model_t );
@@ -33,11 +46,11 @@ md5model_p parse_header( parse_p P ) {
 static 
 parse_p parse_float3( parse_p P, float4* pos) {
 	
-	matchc( skipws(P), '(' );
-	decimalf( skipws(P), &pos->x );
-	decimalf( skipws(P), &pos->y );
-	decimalf( skipws(P), &pos->z ); 
-	matchc( skipws(P), ')' ); 
+	matchc( ff(P), '(' );
+	decimalf( ff(P), &pos->x );
+	decimalf( ff(P), &pos->y );
+	decimalf( ff(P), &pos->z ); 
+	matchc( ff(P), ')' ); 
 
 	return P;
 
@@ -61,8 +74,8 @@ parse_p parse_quat( parse_p P, float4* q ) {
 static 
 md5model_joint_p parse_joints( parse_p P, md5model_p mdl ) {
 
-	if( !parsok( match( skipws(P), "joints" ) ) ) return NULL;
-	if( !parsok( matchc( skipws(P), '{' ) ) ) return NULL;
+	if( !parsok( match( ff(P), "joints" ) ) ) return NULL;
+	if( !parsok( matchc( ff(P), '{' ) ) ) return NULL;
 
 	md5model_joint_p joints = new_array(mdl, md5model_joint_t, mdl->n_joints );
 	for( int i=0; i<mdl->n_joints; i++ ) {
@@ -70,10 +83,13 @@ md5model_joint_p parse_joints( parse_p P, md5model_p mdl ) {
 		md5model_joint_p joint = &joints[i];
 		int parent_idx;
 
-		qstring( skipws(P), joints, &joint->name );
-		integer( skipws(P), &parent_idx );
-		parse_float3( skipws(P), &joint->pos ); joint->pos.w = 1.0f;
-		parse_quat( skipws(P), &joint->orient );
+		qstring( ff(P), joints, &joint->name );
+		integer( ff(P), &parent_idx );
+		if( parent_idx >= 0 )
+			joint->parent = &joints[parent_idx];
+
+		parse_float3( ff(P), &joint->pos ); joint->pos.w = 1.0f;
+		parse_quat( ff(P), &joint->orient );
 
 		if( !parsok(P) ) {
 			delete( joints );
@@ -82,7 +98,7 @@ md5model_joint_p parse_joints( parse_p P, md5model_p mdl ) {
 
 	}
 
-	if( !parsok( matchc( skipws(P), '}' ) ) ) {
+	if( !parsok( matchc( ff(P), '}' ) ) ) {
 		delete(joints);
 		return NULL;
 	}
@@ -94,10 +110,10 @@ md5model_joint_p parse_joints( parse_p P, md5model_p mdl ) {
 static
 parse_p parse_texcoord( parse_p P, float* s, float* t ) {
 	
-	matchc( skipws(P), '(' );
-	decimalf( skipws(P), s );
-	decimalf( skipws(P), t );
-	matchc( skipws(P), ')' );
+	matchc( ff(P), '(' );
+	decimalf( ff(P), s );
+	decimalf( ff(P), t );
+	matchc( ff(P), ')' );
 	
 	return P;
 	
@@ -106,8 +122,8 @@ parse_p parse_texcoord( parse_p P, float* s, float* t ) {
 static
 parse_p parse_mesh( parse_p P, const void* pool, md5model_joint_p joints, md5model_mesh_p mesh ) {
 
-		qstring( skipws( match( skipws(P), "shader" ) ), pool,   &mesh->shader );
-		integer( skipws( match( skipws(P), "numverts" ) ), &mesh->n_verts );
+		qstring( ff( match( ff(P), "shader" ) ), pool,   &mesh->shader );
+		integer( ff( match( ff(P), "numverts" ) ), &mesh->n_verts );
 		if( !parsok(P) ) 
 			return P;
 
@@ -116,18 +132,18 @@ parse_p parse_mesh( parse_p P, const void* pool, md5model_joint_p joints, md5mod
 
 			int idx; 
 
-			match( skipws(P), "vert" );
-			integer( skipws(P), &idx );
-			parse_texcoord( skipws(P), &mesh->verts[idx].s, &mesh->verts[idx].t );
-			integer( skipws(P), &mesh->verts[idx].start_weight );
-			integer( skipws(P), &mesh->verts[idx].n_weights );
+			match( ff(P), "vert" );
+			integer( ff(P), &idx );
+			parse_texcoord( ff(P), &mesh->verts[idx].s, &mesh->verts[idx].t );
+			integer( ff(P), &mesh->verts[idx].start_weight );
+			integer( ff(P), &mesh->verts[idx].n_weights );
 
 			if( !parsok(P) ) 
 				return P;
 
 		}
 		
-		integer( skipws( match( skipws(P), "numtris" ) ), &mesh->n_tris );
+		integer( ff( match( ff(P), "numtris" ) ), &mesh->n_tris );
 		if( !parsok(P) ) 
 			return P;
 
@@ -136,18 +152,18 @@ parse_p parse_mesh( parse_p P, const void* pool, md5model_joint_p joints, md5mod
 
 			int idx; 
 
-			match( skipws(P), "tri" );
-			integer( skipws(P), &idx );
-			integer( skipws(P), &mesh->tris[3*idx + 0] );
-			integer( skipws(P), &mesh->tris[3*idx + 1] );
-			integer( skipws(P), &mesh->tris[3*idx + 2] );
+			match( ff(P), "tri" );
+			integer( ff(P), &idx );
+			integer( ff(P), &mesh->tris[3*idx + 0] );
+			integer( ff(P), &mesh->tris[3*idx + 1] );
+			integer( ff(P), &mesh->tris[3*idx + 2] );
 
 			if( !parsok(P) ) 
 				return P;
 
 		}
 
-		integer( skipws( match( skipws(P), "numtris" ) ), &mesh->n_weights );
+		integer( ff( match( ff(P), "numweights" ) ), &mesh->n_weights );
 		if( !parsok(P) ) 
 			return P;
 
@@ -156,11 +172,11 @@ parse_p parse_mesh( parse_p P, const void* pool, md5model_joint_p joints, md5mod
 
 			int idx, joint;
 
-			match( skipws(P), "weight" );
-			integer( skipws(P), &idx );
-			integer( skipws(P), &joint ); mesh->weights[i].joint = &joints[joint];
-			decimalf( skipws(P), &mesh->weights[i].bias );
-			parse_float3( skipws(P), &mesh->weights[i].pos ); mesh->weights[i].pos.w = 1.0f;
+			match( ff(P), "weight" );
+			integer( ff(P), &idx );
+			integer( ff(P), &joint ); mesh->weights[i].joint = &joints[joint];
+			decimalf( ff(P), &mesh->weights[i].bias );
+			parse_float3( ff(P), &mesh->weights[i].pos ); mesh->weights[i].pos.w = 1.0f;
 
 		}
 
@@ -173,13 +189,13 @@ md5model_mesh_p parse_meshes( parse_p P, md5model_p mdl ) {
 	md5model_mesh_p meshes = new_array(mdl, md5model_mesh_t, mdl->n_meshes );
 	for( int i=0; i<mdl->n_meshes; i++ ) {
 
-		match( skipws(P), "mesh" );
-		matchc( skipws(P), '{' );
+		match( ff(P), "mesh" );
+		matchc( ff(P), '{' );
 
 		md5model_mesh_p mesh = &meshes[i];
 
 		parse_mesh( P, meshes, mdl->joints, mesh );
-		if( !parsok( matchc( skipws(P), '}' ) ) ) {
+		if( !parsok( matchc( ff(P), '}' ) ) ) {
 			delete(meshes);
 			return NULL;
 		}
@@ -194,9 +210,11 @@ static
 md5model_p parse_md5_model( parse_p P ) {
 
 	md5model_p mdl = parse_header(P);
-	
-	mdl->joints = parse_joints(P, mdl);
-	mdl->meshes = parse_meshes(P, mdl);
+
+	if( mdl ) {
+		mdl->joints = parse_joints(P, mdl);
+		mdl->meshes = parse_meshes(P, mdl);
+	}
 
 	return mdl;
 
@@ -249,10 +267,46 @@ resource_p load_resource_MD5( int sz, const void* data ) {
 
 #ifdef __res_md5_TEST__
 
+#include <string.h>
+
 int main( int argc, char* argv[] ) {
 
-	
+	register_loader_RES( "md5mesh", load_resource_MD5 );
+	add_path_RES( "file", "${PWD}" );
 
+	if( argc < 2 ) {
+		printf("Usage:\t%s <path to .md5mesh>\n", argv[0]);
+		return 1;
+	}
+
+	md5model_p mdl = resource(md5model_t,argv[1]);
+	if( !mdl ) {
+		printf("Failed to load: %s\n", argv[1]);
+		return 255;
+	}
+
+	printf("md5stat %s:\n\n", argv[1]);
+	printf("version:\t%d\n", mdl->version);
+	printf("n_joints:\t%d\n", mdl->n_joints);
+	printf("n_meshes:\t%d\n", mdl->n_meshes);
+
+	printf("joints:\n");
+	for( int i=0; i<mdl->n_joints; i++ ) {
+
+		md5model_joint_p joint = &mdl->joints[i];	
+		printf("\t% 3d: %s (%s)\n", i, joint->name, joint->parent ? joint->parent->name : "none");
+
+	}
+
+	printf("meshes:\n");
+	for( int i=0; i<mdl->n_meshes; i++ ) {
+
+		md5model_mesh_p mesh = &mdl->meshes[i];	
+		printf("\t% 3d: verts: % 4d\ttris: % 4d\tweights: % 4d\n", i, mesh->n_verts, mesh->n_tris, mesh->n_weights);
+
+	}
+
+	return 0;
 }
 
 #endif
