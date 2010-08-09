abfe0f1eaa3c5a5a44853759c53d35e0d60abf59
kidmeier <kid.meier@gmail.com>
2010-08-09 00:38:10 -0400

    * flo.c; Implement demo of new event system

--- src/flo.c
+++ src/flo.c
@@ -2,12 +2,9 @@
 #include <stdlib.h>
 
 #include <SDL/SDL.h>
-#include "display.core.h"
 
-#ifndef __TEST__
+int init_FLO( void ) {
 
-int main(int argc, char* argv[]) {
-	
 	// Initialize SDL
 	if( SDL_Init( SDL_INIT_NOPARACHUTE ) < 0 ) {
 		
@@ -15,7 +12,25 @@ int main(int argc, char* argv[]) {
 		exit(1);
 		
 	}
-	
+
+	return 0;
+
+}
+
+#ifndef __TEST__
+
+#include "control.maybe.h"
+#include "display.core.h"
+#include "ev.core.h"
+#include "job.core.h"
+#include "sync.condition.h"
+#include "sync.mutex.h"
+#include "sync.thread.h"
+
+int main(int argc, char* argv[]) {
+
+	int ret = init_FLO();
+
 	if( set_DISPLAY( 512, 288, 0, 0,
 	                 redBits, 8,
 	                 greenBits, 8,
@@ -32,22 +47,29 @@ int main(int argc, char* argv[]) {
 		exit(1);
 	}
 	
-	SDL_Event ev;
-	while( SDL_WaitEvent(&ev) >= 0 ) {
-		
-		switch(ev.type) {
-			
-		case SDL_QUIT: {
-			printf("quitting\n");
-			exit(0);
-			break;
-		}
-			
-		default:
-			break;
-		}
+	ret = maybe( ret, < 0, init_EV() );
+	ret = maybe( ret, < 0, init_JOBS() );
+
+	mutex_t mutex;
+	condition_t signal;
+
+	ret = maybe( ret, < 0, init_MUTEX(&mutex) );
+	ret = maybe( ret, < 0, init_CONDITION(&signal) );
+
+	struct ev_channel_s* keyb = open_EV( evKeyboard );
+	struct ev_channel_s* mouse = open_EV( evMouse );
+	while( !(ret < 0) ) {
+
+		int events = pump_EV();
+		sleep_THREAD( usec_perSecond / 30 );
+
 	}
+	join_deadline_JOB( 0, &mutex, &signal );
 	
+	destroy_MUTEX(&mutex);
+	destroy_CONDITION(&signal);
+
+	return 0;
 }
 
 #endif
