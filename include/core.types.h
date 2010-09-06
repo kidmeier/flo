16ff2d95809228458af3b54a56dd114243062ff5
kidmeier <kid.meier@gmail.com>
2010-09-06 17:23:54 -0400

    * core.types.h; base off of C99 stdint.h, remove string type alias

--- include/core.types.h
+++ include/core.types.h
@@ -1,17 +1,17 @@
 #ifndef __core_types_H__
 #define __core_types_H__
 
-#include <sys/types.h>
+#include <stdint.h>
 
 // Sized integer types
-typedef int8_t    int8;
-typedef u_int8_t  uint8;
-typedef int16_t   int16;
-typedef u_int16_t uint16;
-typedef int32_t   int32;
-typedef u_int32_t uint32;
-typedef int64_t   int64;
-typedef u_int64_t uint64;
+typedef int8_t   int8;
+typedef uint8_t  uint8;
+typedef int16_t  int16;
+typedef uint16_t uint16;
+typedef int32_t  int32;
+typedef uint32_t uint32;
+typedef int64_t  int64;
+typedef uint64_t uint64;
 
 // Convenient aliases
 typedef unsigned int   uint;
@@ -22,7 +22,6 @@ typedef unsigned long  ulong;
 
 // Pointer type aliases
 typedef void* pointer;
-typedef char* string;
 
 // Bool
 #include <stdbool.h>
