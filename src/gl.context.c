d3d5bb99be89c70afa9c1a39e8852ed9f3ab7456
Michael Reid <kid.meier@gmail.com>
2011-04-12 20:38:51 -0400

    * gl.context.c - use debug instead of trace

--- src/gl.context.c
+++ src/gl.context.c
@@ -19,17 +19,17 @@ Glcontext create_Glcontext( Display* dpy ) {
 
 	} else {
 
-		trace("initialized GLEW v%s", glewGetString(GLEW_VERSION));
-		if( GLEW_VERSION_1_1 ) trace0("have OpenGL v1.1");
-		if( GLEW_VERSION_1_2 ) trace0("have OpenGL v1.2");
-		if( GLEW_VERSION_1_3 ) trace0("have OpenGL v1.3");
-		if( GLEW_VERSION_1_4 ) trace0("have OpenGL v1.4");
-		if( GLEW_VERSION_1_5 ) trace0("have OpenGL v1.5");
-		if( GLEW_VERSION_2_0 ) trace0("have OpenGL v2.0");
-		if( GLEW_VERSION_2_1 ) trace0("have OpenGL v2.1");
-		if( GLEW_VERSION_3_0 ) trace0("have OpenGL v3.0");
-		if( GLEW_VERSION_3_1 ) trace0("have OpenGL v3.1");
-		if( GLEW_VERSION_3_2 ) trace0("have OpenGL v3.2");
+		debug("initialized GLEW v%s", glewGetString(GLEW_VERSION));
+		if( GLEW_VERSION_1_1 ) debug0("have OpenGL v1.1");
+		if( GLEW_VERSION_1_2 ) debug0("have OpenGL v1.2");
+		if( GLEW_VERSION_1_3 ) debug0("have OpenGL v1.3");
+		if( GLEW_VERSION_1_4 ) debug0("have OpenGL v1.4");
+		if( GLEW_VERSION_1_5 ) debug0("have OpenGL v1.5");
+		if( GLEW_VERSION_2_0 ) debug0("have OpenGL v2.0");
+		if( GLEW_VERSION_2_1 ) debug0("have OpenGL v2.1");
+		if( GLEW_VERSION_3_0 ) debug0("have OpenGL v3.0");
+		if( GLEW_VERSION_3_1 ) debug0("have OpenGL v3.1");
+		if( GLEW_VERSION_3_2 ) debug0("have OpenGL v3.2");
 
 	}
 
